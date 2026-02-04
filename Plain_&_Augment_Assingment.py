fc26b0e81ed265fec21bd65374ccbb05359f34ea
yashwantschavhan8055-prog <yashwantschavhan8055@gmail.com>
2026-02-04 14:53:35 +0530

    Assignments

---
+++ Plain_&_Augment_Assingment.py
@@ -0,0 +1,16 @@
+a=2   #plain Assingment
+print(a)
+a=2+3+4
+print(a)
+a=2
+a+=3 # Augmented Assingment
+print(a)
+a-=7 #Aug
+print(a)
+a*=7#Aug
+print(a)
+a=a+14 #plain 
+print(a)
+a=5
+a*=8#Aug 
+print(a )
