ce0cb017a2a3f074d46f476910652a5ffc1f7b10
yashwantschavhan8055-prog <yashwantschavhan8055@gmail.com>
2026-02-04 14:39:21 +0530

    Type References

---
+++ Data_Type_References.py
@@ -0,0 +1,33 @@
+a=4.9 #predefined Data Type
+print(a)
+print(type(a))
+b= True#predefined Data Type
+print(b)
+print(type(b))
+a=1j
+print(a)
+print(type(a))
+a=["apple","banana","cherry"]
+print(a)
+print(type(a))
+a=("apple","banana","cherry")
+print(a)
+print(type(a))
+a=range(6)
+print(a)
+print(type(a ))
+a={"name":"John","age":36}
+print(a)
+print(type(a))
+a={"apple","banana","cherry"} 
+print(a)
+print(type(a))
+a=frozenset({"apple","banana","cherry"})
+print(a)
+print(type(a))
+a=b"hello"
+print(a)
+print(type(a))
+a=bytearray(5)
+print(a)
+print(type(a))
