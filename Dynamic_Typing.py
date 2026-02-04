ab8dd57d5c6a3ee2be9607f3e0dd85994286c7d1
yashwantschavhan8055-prog <yashwantschavhan8055@gmail.com>
2026-02-04 14:52:19 +0530

    Dynamic Typing

---
+++ Dynamic_Typing.py
@@ -0,0 +1,4 @@
+a=4
+print(type(a))
+a="hello"
+print(type(a))#Thats Called as Dynamic Programing Declaration is not required it change atomaticaly at runtime .
---
+++ PythonDynamicPrograming.py
@@ -0,0 +1,6 @@
+a=2
+print(type(a))
+a=3.5
+print(type(a))
+a="Hello,INDIA"
+print(type(a))
