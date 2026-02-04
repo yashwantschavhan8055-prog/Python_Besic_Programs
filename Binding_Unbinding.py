52d011a6b83d9444640a98e9669f25baae4cded7
yashwantschavhan8055-prog <yashwantschavhan8055@gmail.com>
2026-02-04 14:36:46 +0530

    Binding_Unbinding
    What is Binding And Unbinding

---
+++ Binding_Unbinding.py
@@ -0,0 +1,8 @@
+a=2 #BINDING
+print(a)
+a=3.4 #Binding
+print(a)
+del a #Unbinding That Method deleting the assinment and free the Space Thats Called As (Garbage Collaction )
+a="Hello" #Binding 
+print(a)
+
---
+++ Type_Cast.py
@@ -0,0 +1,34 @@
+#Type Casting 
+a=int(4.9)
+print(type(a))
+print(a)
+
+#no type cast
+b=str("hellow")#int its Error
+print(b)
+print(type(b))
+
+
+#Type Cast
+b=int("123")
+print(b)
+print(type(b))
+
+#Type Cast
+b=str(123)
+print(b)
+print(type(b))
+
+#All Are Type Cast boolean
+b=bool("hello")
+print(b)
+print(type(b))
+
+b=bool(0)
+print(b)
+print(type(b))
+
+b=bool(-12.3)
+print(b)
+print(type(b))
+
