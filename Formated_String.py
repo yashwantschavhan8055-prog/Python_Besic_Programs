cd2b7d30d30d5649768e00416a7bd76dc594510b
yashwantschavhan8055 <yashwantschavhan8055@gmail.com>
2026-05-22 16:20:12 +0530

    Formated_String.py

---
+++ Formated_String.py
@@ -0,0 +1,4 @@
+a = "Hello"
+b = "Fraiends"
+c = a+b
+print(f'sum of {a !r :10} and {b !r :10} = {c !r :10}')
---
+++ Simple_Intrest_Program_Using_Function.py
@@ -0,0 +1,5 @@
+def fun(p, t, r):
+    return (p * t * r) /100
+p , t , r = 8, 6 ,8
+res = fun(p,t,r)
+print(res)
